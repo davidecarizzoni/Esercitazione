350b7e6dfa60bfd56fb57e865df584879db095b6
davidecarizzoni <davidecarizzoni@gmail.com>
2020-03-23 10:23:48 +0100

    creazione componenti e ngSwitch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { GameListComponent } from './components/game-list/game-list.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { GameDetailComponent } from './components/game-detail/game-detail.component';
+import { EditGameComponent } from './components/edit-game/edit-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameListComponent,
+    MenuComponent,
+    GameDetailComponent,
+    EditGameComponent
   ],
   imports: [
     BrowserModule
