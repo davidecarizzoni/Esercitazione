75c88bb23769ffa4e623be94c3522f191cacf0d7
davidecarizzoni <davidecarizzoni@gmail.com>
2020-03-23 21:21:46 +0100

    creato routing in app.module.ts

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,3 @@
+//FILE ESTERNO PER ROUTING - permette di avere una gerarchia più ordinata, efficiente con molti component
+
+import { RouterModule, Routes } from '@angular/router';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { GameListComponent } from './components/game-list/game-list.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { GameDetailComponent } from './components/game-detail/game-detail.component';
 import { EditGameComponent } from './components/edit-game/edit-game.component';
+
+//dichiarazione della routes, della path dei componenti
+const appRoutes: Routes = [
+  {path: 'home' ,component: HomeComponent},
+  {path: 'menu' ,component: MenuComponent},
+  {path: 'gameList' ,component: GameListComponent},
+  {path: 'gameDetail' ,component: GameDetailComponent},
+  {path:'' ,redirectTo: '/home', pathMatch:'full'}
+  //ADD PAGE NOT FOUND COMPONENT
+]
 
 @NgModule({
   declarations: [
@@ -18,7 +28,10 @@ import { EditGameComponent } from './components/edit-game/edit-game.component';
     EditGameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
