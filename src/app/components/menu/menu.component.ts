b6b023aa6646dca3627ec1a643dffd1ff73b3e4a
davidecarizzoni <davidecarizzoni@gmail.com>
2020-03-23 11:28:24 +0100

    creazione menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MenuItem } from 'src/app/model/menu-item.interface';
 
 @Component({
   selector: 'app-menu',  //poi lo richiamo nell'app menu, per poterlo utilizzare
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+@Output()
+//oggetto di tipo observable, eventi su cui poter pubblicare valori su determinati eventi
+selectMenuItem: EventEmitter<number> = new EventEmitter<number>();
+
+  //dichiarazione variabile pubblica di questo 
+  menuList: MenuItem[] = [
+    {id:1,description:'Home',selected:true},
+    {id:2,description:'Game List',selected:true},
+    {id:3,description:'Edit games',selected:true},
+    {id:4,description:'Games details',selected:true},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectedComponent(id:number){
+    for(let menuItem of this.menuList){
+      menuItem.selected = id === menuItem.id;
+    }
+    this.selectMenuItem.emit(id);
+  }
 }
---
+++ src/app/model/menu-item.interface.ts
@@ -0,0 +1,6 @@
+//definisco il tipo di menu che definisco del component menu
+export interface MenuItem{
+    id: number;
+    description: string;
+    selected: boolean;
+}
