1d46ff92c0d8a0a6d7787e27fa22c0e6c78b5674
davidecarizzoni <davidecarizzoni@gmail.com>
2020-03-30 11:38:09 +0200

    login done

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private loginService:LoginService,private  router:Router){}
+  
+  ngOnInit(): void {}
+
+  login(username:string, password:string){
+    if(this.loginService.isPres(username,password)){
+      this.router.navigateByUrl('/home');
+    }
   }
-
   
 
 }
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { User } from '../model/user-item.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private controllo:boolean;
+
+  constructor() {}
+
+  private userList: User[] = [
+    {username:'Davide', password:'12345'},
+    {username:'Luca', password:'1234'},
+    {username:'Andrea', password:'123'}
+  ];
+
+  isPres(username: string, password: string): boolean {
+    this.controllo  = false;
+    this.userList.forEach(element => {
+      if(element.username === username && element.password === password){
+        this.controllo = true;
+      } 
+    });
+    return this.controllo;
+  }
+}
